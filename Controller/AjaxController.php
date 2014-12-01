2a6f1f8b1dd98162ea16f726eff854f8d8c9780a
Nicolas <nikkidbz@gmail.com>
2014-12-01 18:07:31 +0100

    some fixes

---
+++ Controller/AjaxController.php
@@ -0,0 +1,18 @@
+<?php
+namespace Nhacsam\DataTablesBundle\Controller;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Controller of ajax request from the dataTables
+ *
+ * @author Nhacsam
+ */
+class AjaxController
+{
+    
+   
+
+
+}
